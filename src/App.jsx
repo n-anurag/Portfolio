84b3f1e3702aa36388b276aee9487b0b1e496365
n-anurag <anuragneupane58@gmail.com>
2024-03-13 22:31:47 +0545

    portfolio navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>PortfoliO Website</h1>
+   
+      <div className="header">
+      <div className="logo"></div>
+    
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Project</li>
+            <li>Contact</li>
+          </ul>
+
+          
+        </div>
+        <div className="content">
+          <h2>Hi I am Anurag Neupane. I am </h2>
+          <h1>Web Developer</h1>
+
+          <button>Contact Me</button>
+          <button>Download CV</button>
+        
+        </div>
+       
+     
+    
+      
     </>
   )
 }
